58156ffe7a3b9ee9ccbf46caf1335b9c274550e2
Maria Ramirez <mj161099@gmail.com>
2024-11-17 15:19:02 -0400

    Modificacion de archivo init debido a la eliminacion de los scripts de artistas.

--- Spotipy/__init__.py
+++ Spotipy/__init__.py
@@ -4,10 +4,7 @@ from .album import Album
 from .artista import Artista 
 from .autenticacion_usuario import AutenticacionUsuario 
 from .biblioteca import Biblioteca 
-from .bruno_mars import BrunoMars 
 from .cancion import Cancion 
-from .luis_fonsi import LuisFonsi 
-from .mariah_carey import MariahCarey 
 from .media import Media 
 from .menu import Menu 
 from .playlist import Playlist 
